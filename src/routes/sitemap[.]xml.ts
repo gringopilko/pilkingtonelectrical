1408c52176d8b6caee3e9b574ff6a1e0a5dd6ce8
gringopilko <shanepilkington@gmail.com>
2026-06-21 10:37:55 +1000

    https://www.pilkingtonelectrical.com.au/

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-const BASE_URL = "https://pilkingtonelectrical.com.au";
+const BASE_URL = https://www.pilkingtonelectrical.com.au/
 
 interface SitemapEntry {
   path: string;
