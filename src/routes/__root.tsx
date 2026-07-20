da30abe76832eccb7936c78288862e42bbd47bec
gringopilko <shanepilkington@gmail.com>
2026-07-21 09:09:35 +1000

    Update __root.tsx
    function RootComponent() {
      const { queryClient } = Route.useRouteContext();
    
      return (
        <QueryClientProvider client={queryClient}>
          <Toaster richColors position="top-center" />
          {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
          <Outlet />
        </QueryClientProvider>
      );
    }

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,8 +8,8 @@ import {
   HeadContent,
   Scripts,
 } from "@tanstack/react-router";
-import { useEffect, type ReactNode } from "react";
-
+import { useEffect, type ReactNode } from "react"; 
+import { Toaster } from "@/components/ui/sonner";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
@@ -170,6 +170,7 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster richColors position="top-center" />
       {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
     </QueryClientProvider>
