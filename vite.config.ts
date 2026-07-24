6822c0d5506e971f3ab980ec209ace327e6a9ec8
gringopilko <shanepilkington@gmail.com>
2026-07-25 09:15:17 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
     viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
-    tanstackStart({
-      target: "node",
-    }),
+    tanstackStart(),
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     viteReact(),
   ],
 });
