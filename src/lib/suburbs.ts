816ef2a5526f890cfa16900eb00f15fa13b4e03d
gringopilko <shanepilkington@gmail.com>
2026-07-27 11:02:44 +1000

    Create suburbs.ts

---
+++ src/lib/suburbs.ts
@@ -0,0 +1,29 @@
+export const suburbGroups = [
+  {
+    area: "Inner Bayside",
+    suburbs: ["Elwood", "St Kilda", "St Kilda East", "St Kilda West", "Balaclava", "Windsor", "Prahran", "South Yarra", "Albert Park", "Middle Park", "South Melbourne", "Port Melbourne"],
+  },
+  {
+    area: "Bayside",
+    suburbs: ["Brighton", "Brighton East", "Hampton", "Hampton East", "Black Rock", "Beaumaris", "Mentone", "Mordialloc"],
+  },
+  {
+    area: "Stonnington & Boroondara",
+    suburbs: ["Malvern", "Malvern East", "Glen Iris", "Camberwell", "Caulfield", "Caulfield North", "Caulfield South"],
+  },
+  {
+    area: "South East",
+    suburbs: [
+      "Bentleigh", "Bentleigh East", "Moorabbin", "Cheltenham", "Dingley Village",
+      "Murrumbeena", "Oakleigh", "Clayton", "Clayton South", "Clarinda",
+      "Springvale", "Noble Park", "Keysborough",
+    ],
+  },
+  {
+    area: "Outer South East & Bay",
+    suburbs: [
+      "Aspendale", "Aspendale Gardens", "Braeside", "Waterways", "Bangholme",
+      "Patterson Lakes", "Seaford", "Carrum Downs",
+    ],
+  },
+];
