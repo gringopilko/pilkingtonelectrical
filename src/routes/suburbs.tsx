10b111543213d7a428c70dd83e1ea38643f1fd2a
gringopilko <shanepilkington@gmail.com>
2026-07-27 11:03:23 +1000

    Update suburbs.tsx

--- src/routes/suburbs.tsx
+++ src/routes/suburbs.tsx
@@ -3,36 +3,7 @@ import { MapPin, Phone } from "lucide-react";
 import { Nav } from "@/components/Nav";
 import { Footer } from "@/components/Footer";
 import { QuoteForm } from "@/components/QuoteForm";
-
-const suburbGroups = [
-  {
-    area: "Inner Bayside",
-    suburbs: ["Elwood", "St Kilda", "St Kilda East", "St Kilda West", "Balaclava", "Windsor", "Prahran", "South Yarra", "Albert Park", "Middle Park", "South Melbourne", "Port Melbourne"],
-  },
-  {
-    area: "Bayside",
-    suburbs: ["Brighton", "Brighton East", "Hampton", "Hampton East", "Black Rock", "Beaumaris", "Mentone", "Mordialloc"],
-  },
-  {
-    area: "Stonnington & Boroondara",
-    suburbs: ["Malvern", "Malvern East", "Glen Iris", "Camberwell", "Caulfield", "Caulfield North", "Caulfield South"],
-  },
-  {
-    area: "South East",
-    suburbs: [
-      "Bentleigh", "Bentleigh East", "Moorabbin", "Cheltenham", "Dingley Village",
-      "Murrumbeena", "Oakleigh", "Clayton", "Clayton South", "Clarinda",
-      "Springvale", "Noble Park", "Keysborough",
-    ],
-  },
-  {
-    area: "Outer South East & Bay",
-    suburbs: [
-      "Aspendale", "Aspendale Gardens", "Braeside", "Waterways", "Bangholme",
-      "Patterson Lakes", "Seaford", "Carrum Downs",
-    ],
-  },
-];
+import { suburbGroups } from "@/lib/suburbs";
 
 export const Route = createFileRoute("/suburbs")({
   head: () => ({
